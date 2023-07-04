8bebc6ebb518d34c77bf559434e4e0037e70cf7d
Louis Parent <contact@louis-parent.fr>
2023-07-04 23:51:34 +0200

    Adding animation for double face card

--- scripts/collection.js
+++ scripts/collection.js
@@ -6,20 +6,59 @@ document.addEventListener("DOMContentLoaded", () => {
 		container.style.position = "relative";
 		container.style.width = "15vw";
 		container.style.minWidth = "256px";
+		container.style.height = "20.92vw";
+		container.style.minHeight = "357px";
 		container.style.margin = "1vw";
 		container.style.display = "inline-block";
 		
-		const img = document.createElement("img");
 		item.newestCard.then(card => {
-			img.alt = card.oracle.name;
-			img.src = card.faces[0];
+			const imgRecto = document.createElement("img");
+			imgRecto.classList.add("full-width", "recto");
+			imgRecto.alt = card.oracle.name.split("//")[0].trim();
+			imgRecto.src = card.faces[0];
+			imgRecto.style.position = "absolute";
+			container.appendChild(imgRecto);
+			
+			if(card.faces.length > 1) {
+				const imgVerso = document.createElement("img");
+				imgVerso.classList.add("full-width", "verso");
+				imgVerso.alt = card.oracle.name.split("//")[1].trim();
+				imgVerso.src = card.faces[1];
+				imgVerso.style.display = "none";
+				imgVerso.style.position = "absolute";
+				container.appendChild(imgVerso);
+			
+				const reverse = document.createElement("button");
+				reverse.classList.add("cta", "primary", "floating");
+				reverse.innerText = "⭮";
+				reverse.style.position = "absolute";
+				reverse.style.top = "1%";
+				reverse.style.left = "50%";
+				reverse.style.transform = "translateX(-50%)";
+				reverse.style.zIndex = "100";
+				
+				let isRectoVisible = true;
+				reverse.addEventListener("click", () => {
+					imgVerso.style.display = null;
+					
+					if(isRectoVisible) {
+						imgRecto.style.animation = "fromVisibleToHidden 1s ease-in-out forwards";
+						imgVerso.style.animation = "fromHiddenToVisible 1s ease-in-out forwards";
+						isRectoVisible = false;
+					}
+					else {
+						imgRecto.style.animation = "fromHiddenToVisible 1s ease-in-out forwards";
+						imgVerso.style.animation = "fromVisibleToHidden 1s ease-in-out forwards";
+						isRectoVisible = true;
+					}
+				});
+				
+				container.appendChild(reverse);
+			}
 		});
-		img.style.width = "100%";
-		container.appendChild(img);
 		
-		const pill = document.createElement("span");
+		const pill = document.createElement("div");
 		pill.innerHTML = item.nonFoilCount + " / " + item.foilCount + "<small>✨</small>";
-		pill.style.display = "block";
 		pill.style.position = "absolute";
 		pill.style.left = "50%";
 		pill.style.bottom = "5%";
@@ -28,6 +67,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		pill.style.borderRadius = "1em";
 		pill.style.padding = "0.25em 0.5em";
 		pill.style.transform = "translateX(-50%)";
+		pill.style.zIndex = "100";
 		container.appendChild(pill);
 		
 		document.querySelector("#cards").appendChild(container);
